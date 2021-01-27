34be8f265898a5513c6b006d48d7aa7898dcb8ea
Fernando <fernandoborgesjr@outlook.com>
2021-01-27 13:32:49 -0300

    Fixes

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -23,19 +23,26 @@ class AuthController extends Controller
     public function login(Request $request)
     {
         if (!filter_var($request->email, FILTER_VALIDATE_EMAIL)) {
-            return redirect()->back()->withInput()->withErrors(['O e-mail informado não é válido!']);
+            $login['success'] = false;
+            $login['message'] = 'O e-mail informado não é válido!';
+            echo json_encode($login);
+            return;
         }
-
 
         $credentials = [
             'email' => $request->email,
             'password' => $request->password
-
         ];
         if (Auth::attempt($credentials, false)) {
-            return view('admin.dashboard');
+            $login['success'] = true;
+            echo json_encode($login);
+            //return view('admin.dashboard');
+            return;
         }
-        return redirect()->back()->withInput()->withErrors(['Os dados informados não conferem!']);
+        $login['success'] = false;
+        $login['message'] = 'Os dados informados não conferem!';
+        echo json_encode($login);
+        return;
     }
 
     public function logout()
--- resources/views/admin/formLogin.blade.php
+++ resources/views/admin/formLogin.blade.php
@@ -11,16 +11,13 @@
 
 <body>
     <div class="container">
-    
-        <form class="form-signin form" method="post" action="{{ route('admin.login.do') }}">
+
+        <form class="form-signin form" id="formLogin">
             @csrf
-            @if($errors->all())
-            @foreach($errors->all() as $error)
-            <div class="alert alert-danger" role="alert">
-                {{ $error }}
+
+            <div class="alert alert-danger d-none messageBox" role="alert">
             </div>
-            @endforeach
-            @endif
+
             <div class="form-group">
                 <label for="email">E-mail</label>
                 <input type="text" name="email" id="email">
@@ -32,6 +29,34 @@
             <button class="btn btn-primary">Entrar</button>
         </form>
     </div>
+    <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.form/4.3.0/jquery.form.min.js" integrity="sha384-qlmct0AOBiA2VPZkMY3+2WqkHtIQ9lSdAsAn5RUJD/3vA5MKDgSGcdmIv4ycVxyn" crossorigin="anonymous"></script>
+    <script>
+        $(function() {
+            $('#formLogin').submit(function(e) {
+                e.preventDefault();
+                $.ajax({
+                    //A rota ara qual será enviado o formulário
+                    url: "{{ route('admin.login.do') }}",
+                    type: "post",
+                    /*Esse serialize verifica todos os campos do formulário
+                     * criando chave:valor
+                     */
+                    data: $(this).serialize(),
+                    //Indica o retorno esperado
+                    dataType: 'json',
+                    //Função que será executada caso tenha sucesso na requisição
+                    success: function(resp) {
+                        
+                        if (resp.success === true) {
+                            window.location.href = "{{ route('admin') }}"
+                        }
+                        $('.messageBox').removeClass('d-none').html('<span>' + resp.message + '</span>');
+                    }
+                });
+            });
+        });
+    </script>
 </body>
 
 </html>
